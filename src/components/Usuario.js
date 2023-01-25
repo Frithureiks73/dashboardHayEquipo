661bccd42a4567aa94d9cc1809b9ce296f23b64f
Frithureiks73 <fedex_73@hotmail.com>
2023-01-24 21:47:25 -0300

    Mostrando imagen de Usuario en Dash online

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -6,7 +6,7 @@ function Usuario(props) {
         <React.Fragment>
             <div className="card-body">
                 <div className="text-center">
-                    <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: 40 + 'rem' }} src={props.foto_perfil ? "http://localhost:3000/img/img-users/" + props.foto_perfil : imagenPerfil} alt="Foto de Perfil" />
+                    <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: 40 + 'rem' }} src={props.foto_perfil ? "https://www.hayequipo-4o7m.onrender.com/img/img-users/" + props.foto_perfil : imagenPerfil} alt="Foto de Perfil" />
                 </div>
                 <p className='font-weight-bold'>¡Bienvenido {props.nombre} {props.apellido}!</p>
                 <p className='font-weight-bold'>D.N.I.:</p><p> {props.dni} </p>
